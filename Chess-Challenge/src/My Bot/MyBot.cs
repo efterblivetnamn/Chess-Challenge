5c9cf41516c5c4ae1900cb2b3bd37e479f987eaa
efterblivetnamn <105668078+efterblivetnamn@users.noreply.github.com>
2023-08-16 20:19:16 +0200

    made changes to improve it, now it's somehow even worse

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,65 +8,64 @@ using System.Collections;
 // Docs:                     https://seblague.github.io/chess-coding-challenge/documentation/
 public class MyBot : IChessBot
 {
-  
     bool botColor;
-    Hashtable transPosTable = new Hashtable();
 
     public Move Think(Board board, Timer timer)
     {
         
         botColor = board.IsWhiteToMove;
         Move moveToPlay = new();
-        //bool val = false;
-        //System.Console.WriteLine(board.CreateDiagram(val));
-
 
         Move[] LegalMoves = board.GetLegalMoves();
         Quicksort(LegalMoves, 0, LegalMoves.Length - 1);
-        double bestEval = double.MinValue;
 
-        for (int i = 0; i < LegalMoves.Length; i++)
-        {   
-            board.MakeMove(LegalMoves[i]);
-
-            if (board.IsInCheckmate() )
+        int bestEval = int.MinValue;
+        
+            foreach (Move move in LegalMoves)
             {
-                Console.WriteLine("We Fuking Won");
-                return LegalMoves[i];
-            }
 
-            double currentEval = MaxMin(board, 3, double.MinValue, double.MaxValue, false);
-            board.UndoMove(LegalMoves[i]);
-            if (currentEval >= bestEval)
-            {
-                bestEval = currentEval;
-                moveToPlay = LegalMoves[i];
+                
+
+                board.MakeMove(move);
+                if (board.IsInCheckmate() )
+                {
+                    Console.WriteLine("We Fuking Won");
+                    return move;
+                }
+                int currentEval = MaxMin(board, 3, int.MinValue, int.MaxValue, false, timer);
+                board.UndoMove(move);
+                if (currentEval >= bestEval)
+                {
+                    bestEval = currentEval;
+                    moveToPlay = move;
+                }
             }
-        }
+        
 
         return moveToPlay;
     }
 
-    double MaxMin(Board board, int depth, double alpha, double beta, bool maxPlayer)
+    int MaxMin(Board board, int depth, int alpha, int beta, bool maxPlayer, Timer timer)
     {
         if (board.IsDraw()) return 0;
-        if (board.IsInCheckmate()) return !maxPlayer? double.MaxValue : double.MinValue;
         
+        int maxEval = maxPlayer? int.MinValue : int.MaxValue;
+        if (board.IsInCheckmate()) return maxEval;
         
         Move[] legalMoves = board.GetLegalMoves();
         
-        if (depth == 0) {
-            return Quiesce(board, legalMoves, alpha, beta);
-        }
+        if (depth == 0) return Quiesce(board, legalMoves, alpha, beta, timer);
        
-        double maxEval = maxPlayer? double.MinValue : double.MaxValue;
-       
+
         Quicksort(legalMoves, 0, legalMoves.Length - 1);
         foreach (Move move in legalMoves){
 
+            
+          
             board.MakeMove(move);
-            double eval = MaxMin(board, depth - 1 , alpha, beta, !maxPlayer);
+            int eval = MaxMin(board, depth - 1 , alpha, beta, !maxPlayer, timer);
             board.UndoMove(move);
+
             if (maxPlayer)
             {
                 maxEval = Math.Max(maxEval, eval);
@@ -76,35 +75,44 @@ public class MyBot : IChessBot
                 maxEval = Math.Min(maxEval, eval);
                 beta = Math.Min(beta, eval);
             }
+           
             if (beta <= alpha) break;
+
+         
         }
 
-        
         return maxEval;
     }
 
-    double Quiesce(Board board, Move[] legalMoves, double alpha, double beta ) {
-        double stand_pat = EvalPos(board);
-        if( stand_pat >= beta )
+    int Quiesce(Board board, Move[] legalMoves, int alpha, int beta, Timer timer) {
+        int currentPosition = EvalPos(board);
+        if( currentPosition >= beta )
             return beta;
-        if( alpha < stand_pat )
-            alpha = stand_pat;
+        if( alpha < currentPosition )
+            alpha = currentPosition;
+
+        //Quicksort(legalMoves, 0, legalMoves.Length-1);
 
         foreach (Move move in legalMoves){
             if (move.IsCapture)
             {
+
+
                 board.MakeMove(move);
-                double score = -Quiesce(board, board.GetLegalMoves(), -beta, -alpha);
+                int score = -Quiesce(board, board.GetLegalMoves(), -beta, -alpha, timer);
                 board.UndoMove(move);
                 
                 if( score >= beta )
                     return beta;
                 if( score > alpha )
                     alpha = score;
+
+          
             }
         }
         return alpha;
     }
+
 
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
     
@@ -116,22 +124,19 @@ public class MyBot : IChessBot
                 0b_0000000011111111000000000000000000000000000000000000000000000000,
                 0b_0000000011111111000000000000000000000000000000001110011100000000,
                 0b_0000000011111111000000000011110000111100000000001110011100000000,
-                0
             },
             {
                 0b_0000000000000000000000000001100000011000000000000000000000000000,
                 0b_0000000000000000001111000011110000111100001111000000000000000000,
                 0b_0000000000011000001111000011110000111100001111000001100000000000, 
-                0,
             },
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
             {
                 0b_0000000000000000000000000000000000000000000000000000000011000011,
                 0b_0000000000000000000000000000000000000000000000001000000111100111,
                 0b_0000000000000000000000000000000000000000000000001110011111100111, 
-                0
             },
         },
         {
@@ -139,38 +144,33 @@ public class MyBot : IChessBot
                 0b_0000000000000000000000000000000000000000000000001111111100000000,
                 0b_0000000011100111000000000000000000000000000000001111111100000000,
                 0b_0000000011100111000000000011110000111100000000001111111100000000,
-                0
             },
             {
                 0b_0000000000000000000000000001100000011000000000000000000000000000,
                 0b_0000000000000000001111000011110000111100001111000000000000000000,
                 0b_0000000000011000001111000011110000111100001111000001100000000000, 
-                0,
             },
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0},
             {
                 0b_1100001100000000000000000000000000000000000000000000000000000000,
                 0b_1110011110000001000000000000000000000000000000000000000000000000,
                 0b_1110011111100111000000000000000000000000000000000000000000000000, 
-                0
             },
         }
     }; 
 
-    double EvalPos(Board board)
+    int EvalPos(Board board)
     {
-  
-  
-        double currentEval = 0;
+        int currentEval = 0;
            
         int botPieceVals = 0;
-        int opPieceVals = 0;
         PieceList[] AllPieces = board.GetAllPieceLists();
 
-        double piecesPositionBonus = 0;
+        int piecesPositionBonus = 0;
         int AttackedSqauresBonus = 0;
+
         foreach (PieceList pieceList in AllPieces)
         {
             PieceType ListType = pieceList.TypeOfPieceInList;
@@ -178,41 +178,46 @@ public class MyBot : IChessBot
               
             int AddVal = pieceValues[ typeAsInt ] * pieceList.Count*2;
               
-            if (pieceList.IsWhitePieceList == botColor)
-            {
+            if (pieceList.IsWhitePieceList == botColor) 
                 botPieceVals += AddVal;
-            }else
-            {
-                opPieceVals += AddVal;
-            }
-
-
+            else 
+                botPieceVals -= AddVal;
+            
             ulong PieceBitBoard = board.GetPieceBitboard(ListType, botColor);
-                
-            for(uint i=0; i<4; i++)
-            {
-                ulong MaskedBoard = PieceBonus[ botColor? 0: 1, typeAsInt-1, i] & PieceBitBoard;
-                piecesPositionBonus += BitboardHelper.GetNumberOfSetBits(MaskedBoard) * 50;
-            }
 
-            if (typeAsInt > 3 && typeAsInt < 6)
+            int m =  (pieceList.IsWhitePieceList == botColor)? 1 : -1;
+            
+            if (typeAsInt >= 3 && typeAsInt < 6)
             {
-                   
+
                 for(int i = 0; i < pieceList.Count; i++)
                 {
                     ulong AttackedSquare = BitboardHelper.GetSliderAttacks(ListType, pieceList.GetPiece(i).Square, board);
-                    AttackedSqauresBonus += BitboardHelper.GetNumberOfSetBits(AttackedSquare) * 20;
+                    
+                    int tmp = BitboardHelper.GetNumberOfSetBits(AttackedSquare) * m * 20;
+
+                    AttackedSqauresBonus += tmp;
+                    
                 }
+            } else {
+
+                for(uint i=0; i<3; i++)
+                {
+
+                    ulong MaskedBoard = PieceBonus[ pieceList.IsWhitePieceList? 0 : 1, typeAsInt-1, i] & PieceBitBoard;
+                    
+                    piecesPositionBonus += BitboardHelper.GetNumberOfSetBits(MaskedBoard) * m * 50;
+
+                }
+
             }
         }
-        currentEval += (botPieceVals-opPieceVals) + piecesPositionBonus + AttackedSqauresBonus;
+        currentEval += botPieceVals + piecesPositionBonus + AttackedSqauresBonus;
 
         /*if (board.IsInCheck())
         {
             //currentEval += 200;
         }*/
-
-        
       
         return currentEval;
     }
@@ -220,36 +225,35 @@ public class MyBot : IChessBot
    
 
     // Quicksort(arr, 0, arr.Length - 1);
-    public static void Quicksort(Move[] arr, int low, int high)
+    void Quicksort(Move[] arr, int low, int high)
     {
         if (low < high)
         {
-            int pivotIndex = Partition(arr, low, high);
+           
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                int v1 = MoveToInt(arr[j]); //  arr[j].IsPromotion ? 0 : (arr[j].IsCapture ? 1 : 2);
+                int v2 = MoveToInt(arr[high]); //arr[high].IsPromotion ? 0 : (arr[high].IsCapture ? 1 : 2);
+                
+                if ( v1 > v2 )
+                {
+                    i++;
+                    (arr[j], arr[i]) = (arr[i], arr[j]);
+                }
+            }
+
+            (arr[high], arr[i+1]) = (arr[i+1], arr[high]);
+
+            int pivotIndex =  i + 1;
 
             Quicksort(arr, low, pivotIndex - 1);
             Quicksort(arr, pivotIndex + 1, high);
         }
     }
 
-    public static int Partition(Move[] arr, int low, int high)
-    {
-        Move pivotValue = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++)
-        {
-
-            int v1 = arr[j].IsPromotion ? 0 : (arr[j].IsCapture ? 1 : 2);
-            int v2 = pivotValue.IsPromotion ? 0 : (pivotValue.IsCapture ? 1 : 2);
-             
-            if ( v1 < v2 )
-            {
-                i++;
-                (arr[j], arr[i]) = (arr[i], arr[j]);
-            }
-        }
-
-        (arr[high], arr[i+1]) = (arr[i+1], arr[high]);
-        return i + 1;
+    int MoveToInt(Move move){
+      return move.IsPromotion ? 6 : (move.IsCapture ? (int)move.CapturePieceType : 0);
     }
 }
